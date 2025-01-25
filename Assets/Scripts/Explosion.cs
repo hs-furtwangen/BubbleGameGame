0a8abdf6191410795bc43fab7234d1a9351bd47b
Alexander Scheurer <mail@aspepex.net>
2025-01-25 22:23:29 +0100

    var explosion

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -1,10 +1,35 @@
 using UnityEngine;
 
+[RequireComponent (typeof(ParticleSystem))]
+[RequireComponent (typeof(ParticlesController))]
 public class Explosion : MonoBehaviour
 {
+    private ParticleSystem ps;
+    private ParticlesController pc;
+
+    public float BaseSize;
+
     public void Start()
     {
+        ps = this.gameObject.GetComponent<ParticleSystem>();
+        pc = this.gameObject.GetComponent<ParticlesController>();
+
+        BubbleValues(Random.value, new Color(Random.value, Random.value, Random.value));
+
         Debug.Log("Destroy init");
         Destroy(this.gameObject, 5);
+    }
+
+    public void BubbleValues(float size, Color color)
+    {
+        var psMain = ps.main;
+        psMain.startColor = color;
+
+        psMain.startSize = size * BaseSize * 2;
+
+        pc.paintColor = color;
+
+        pc.minRadius = size * BaseSize * 0.1f;
+        pc.maxRadius = size * BaseSize * 0.8f;
     }
 }
